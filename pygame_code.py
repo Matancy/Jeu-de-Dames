d3215fb9d4efc5257bb65dc759769c21854efb07
Matancy <matheo.tichy@gmail.com>
2021-04-07 11:06:45 +0200

    Update pygame code

--- pygame_code.py
+++ pygame_code.py
@@ -1,31 +1,82 @@
 import pygame
 
+
+
+
+def plateau(size, couleur1, couleur2):
+    for y in range(5):
+        for i in range(5):
+            pygame.draw.rect(surf, couleur1, (0 + (size/5 * i), 0 + (size/5 * y), size/10, size/10))
+            pygame.draw.rect(surf, couleur2, (size/10 + (size/5 * i), 0 + (size/5 * y), size/10, size/10))
+        for i in range(5):
+            pygame.draw.rect(surf, couleur2, (0 + (size/5 * i), size/10 + (size/5 * y), size/10, size/10))
+            pygame.draw.rect(surf, couleur1, (size/10 + (size/5 * i), size/10 + (size/5 * y), size/10, size/10))
+
+
+def pion(size, couleur, x, y):
+    pygame.draw.circle(surf, couleur, (x, y), int(size/22))
+def x(case):
+    return int((size/20)+(2*case*size/20))
+def y(case):
+    return int((size/20)+(2*case*size/20))
+
+
+def depart(size, couleur1, couleur2):
+    for i in range(5):
+        pion(size, couleur1, x(1+i*2), y(0))
+        pion(size, couleur1, x(0+i*2), y(1))
+        pion(size, couleur1, x(1+i*2), y(2))
+        pion(size, couleur1, x(0+i*2), y(3))
+
+    for i in range(5):
+        pion(size, couleur2, x(1+i*2), y(6))
+        pion(size, couleur2, x(0+i*2), y(7))
+        pion(size, couleur2, x(1+i*2), y(8))
+        pion(size, couleur2, x(0+i*2), y(9))
+
+
+
+
 pygame.init()
 
-size = 800
 
+size = 800
 surf = pygame.display.set_mode((size, size))
-marron_clair = pygame.Color(226, 188, 116)
-marron_fonce = pygame.Color(88, 41, 0)
+
+
+beige1 = pygame.Color(226, 188, 116)
+marron1 = pygame.Color(88, 41, 0)
+beige2 = pygame.Color(230, 200, 150)
+marron2 = pygame.Color(45, 30, 11)
+orange = pygame.Color(152, 87, 23)
+
+
+test = pygame.Color(105, 105, 105)
+
 blanc = pygame.Color(255, 255, 255)
 noir = pygame.Color(0, 0, 0)
 run = True
 
+
+
 while run :
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
-
-    for y in range(5):
-        for i in range(5):
-            pygame.draw.rect(surf,marron_clair,(0 + (size/5 * i), 0 + (size/5 * y), size/10, size/10))
-            pygame.draw.rect(surf,marron_fonce,(size/10 + (size/5 * i), 0 + (size/5 * y), size/10, size/10))
-        for i in range(5):
-            pygame.draw.rect(surf,marron_fonce,(0 + (size/5 * i), size/10 + (size/5 * y), size/10, size/10))
-            pygame.draw.rect(surf,marron_clair,(size/10 + (size/5 * i), size/10 + (size/5 * y), size/10, size/10))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    plateau(size, beige1, marron2)
 
 
-  pygame.display.flip()
+    depart(size, test, orange)
+
+    """
+    pion(size, beige2, x(1), y(0))
+    pion(size, orange, x(0), y(9))
+
+
+    """
+
+
+    pygame.display.flip()
 
 
 
