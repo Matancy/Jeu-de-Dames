502ee471e896fbae28149597b8a8d2e01ca02df4
Matancy <matheo.tichy@gmail.com>
2021-05-10 16:12:29 +0200

    Create file to config the game
    + Add possibility to configure game interface and gaame type

---
+++ config.py
@@ -0,0 +1,13 @@
+#
+# Fichier permettant de configurer la partie
+#
+
+# Mode d'affichage
+
+game_display_type = "console"
+#game_display_type = "graphical"
+
+# Mode de jeu
+
+game_type = "2-players"
+#game_type = "ia"
