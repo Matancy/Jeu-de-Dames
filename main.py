742f1c39af75baa85972947b8e59e3d97d85e0ef
Matancy <matheo.tichy@gmail.com>
2021-05-18 11:48:04 +0200

    Code organisation et commentaires
    * Update code organisation
    + Add and update comments

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from affichage_damier import *
-from jeu import *
+from deplacement import *
 from config import *
 from data import *
 from fin_de_partie import *
 from verification_dame import *
+from manger import *
 import pygame
 
 # Variable de déplacement évitant de bloquer le jeu par la prévision
@@ -11,11 +12,13 @@ deplacement_simple = False
 
 while partie != 5:
 
+
     # Système de fermeture de l'interface graphique
     if game_display_type == "graphical":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 partie = 5
+
 
     # On regarde si le joueur précédent peut encore manger
     prevision = indication_doit_manger(joueur, deplacement_simple)
@@ -30,10 +33,12 @@ while partie != 5:
         # On refais les indications de déplacement avec le nouveau joueur
         prevision = indication_doit_manger(joueur, deplacement_simple)
 
+
     # Affichage du plateau de jeu en fonction de la configuration
     if game_display_type == "console":
         affichage_damier_console(damier)
-    #else:
+    else:
+        pass
         # affichage_damier_graphique(damier)
         # Ne pas oublier de gérer cette zone pour ne pas afficher plusieurs fois le damier
 
@@ -44,7 +49,7 @@ while partie != 5:
     source = str(input("Emplacement source : "))
     destination = str(input("Emplacement destination : "))
 
-    # On déplace le joueur
+    # On effectue les vérifications et on le déplace
     result = quel_deplacement(source, destination, joueur, prevision)
 
     # Si on a une erreur sur le fonctionnement, on fait rejouer le joueur
