ab29b9680155d7c16ad88ead3d264b58a5317340
Matancy <matheo.tichy@gmail.com>
2021-05-19 15:10:20 +0200

    Import code from pygame code to the code

--- affichage_damier.py
+++ affichage_damier.py
@@ -1,10 +1,12 @@
 from data import *
 from config import *
+from gestion_data import *
 import pygame
 import pygame_code
 #
 # Fichier regroupant les fonctions relatives à l'affichage du damier
 #
+
 
 
 # Affichage du damier en mode console
@@ -25,13 +27,20 @@ def affichage_damier_console(damier):
     print("\n")
 
 
-"""
+
 # Affichage du damier en mode graphique
-def affichage_damier_graphique(damier):
+#def affichage_damier_graphique(damier):
   
   #Cette fonction affiche le damier en interface graphique
   #Input (list): damier
   #Return (display): Affichage du damier
+
+  for i in range(10):
+    for j in range(10):
+      if damier[i][j] == 'O':
+        pion(game_display_size, gris, convert_case_x(j), convert_case_y(i))
+      elif damier[i][j] == 'X':
+        pion(game_display_size, orange, convert_case_x(j), convert_case_y(i))
   
 
 # Création du damier en mode graphique
@@ -39,23 +48,13 @@ def creation_plateau():
   return pygame.display.set_mode((game_display_size, game_display_size))
 
 
-# Remplissage du plateau temporaire
-def plateau(size, couleur1, couleur2):
-  plateau = creation_plateau()
-  for y in range(5):
-    for i in range(5):
-      pygame.draw.rect(surf, couleur1, (0 + (size / 5 * i), 0 + (size / 5 * y), size / 10, size / 10))
-      pygame.draw.rect(surf, couleur2, (size / 10 + (size / 5 * i), 0 + (size / 5 * y), size / 10, size / 10))
-    for i in range(5):
-      pygame.draw.rect(surf, couleur2, (0 + (size / 5 * i), size / 10 + (size / 5 * y), size / 10, size / 10))
-      pygame.draw.rect(surf, couleur1, (size / 10 + (size / 5 * i), size / 10 + (size / 5 * y), size / 10, size / 10))
+plateau = creation_plateau() # Variable de plateau
 
 
 # Affichage d'un pion
 def pion(size, couleur, x, y):
   pygame.draw.circle(plateau, couleur, (x, y), int(size / 22))
-def x(case):
-    return int((size/20)+(2*case*size/20))
-def y(case):
-    return int((size/20)+(2*case*size/20))
-    """
+
+
+def init_plateau():
+  pygame.init()
--- pygame_code.py
+++ pygame_code.py
@@ -4,53 +4,24 @@ from data import *
 from gestion_data import *
 
 
-def plateau(size, couleur1, couleur2):
-    for y in range(5):
-        for i in range(5):
-            pygame.draw.rect(surf, couleur1, (0 + (size/5 * i), 0 + (size/5 * y), size/10, size/10))
-            pygame.draw.rect(surf, couleur2, (size/10 + (size/5 * i), 0 + (size/5 * y), size/10, size/10))
-        for i in range(5):
-            pygame.draw.rect(surf, couleur2, (0 + (size/5 * i), size/10 + (size/5 * y), size/10, size/10))
-            pygame.draw.rect(surf, couleur1, (size/10 + (size/5 * i), size/10 + (size/5 * y), size/10, size/10))
 
-
-def pion(size, couleur, x, y):
-    pygame.draw.circle(surf, couleur, (x, y), int(size/22))
-def x(case):
-    return int((size/20)+(2*case*size/20))
-def y(case):
-    return int((size/20)+(2*case*size/20))
-
-
-def dessiner_tableau (size, couleurO, couleurX):
-  for i in range (10):
-    for j in range (10):
-      if damier[i][j] == 'O':
-        pion(size, couleurO, x(j), y(i))
-      elif damier[i][j] == 'X':
-        pion(size, couleurX, x(j), y(i))
-
-def Xvaleur(X):
-  return X//(int(size/10))
-def Yvaleur(Y):
-  return Y//(int(size/10))
 
 joueur = quel_joueur(joueur)
 
 
-pygame.init()
+
 
 
 size = 600
-surf = pygame.display.set_mode((size, size))
+plateau = pygame.display.set_mode((size, size))
 
 
-beige1 = pygame.Color(226, 188, 116)
+
 marron1 = pygame.Color(88, 41, 0)
 beige2 = pygame.Color(230, 200, 150)
 marron2 = pygame.Color(45, 30, 11)
-orange = pygame.Color(152, 87, 23)
-gris = pygame.Color(105, 105, 105)
+
+
 blanc = pygame.Color(255, 255, 255)
 noir = pygame.Color(0, 0, 0)
 run = True
